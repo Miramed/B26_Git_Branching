00ad7f3b484328a1a8994a93396ca60a6506cb96
Miramed <mr.tekin2112@gmail.com>
2022-04-02 13:47:03 -0500

    Created tc002.

---
+++ src/TC002.java
@@ -0,0 +1,19 @@
+public class TC002 {
+
+    public static void main(String[] args) {
+
+        // testing something
+
+
+        // more testing
+
+
+        // negative testing
+
+
+        // positive testing
+
+
+        // checking the failing
+    }
+}
